4656f2d9c7f3997805b4c3300f5762c1273688b1
Ben Goldstein <beng@brownpapertickets.com>
2019-04-04 15:00:48 -0700

    fix bugs

--- src/actor/slack/SlackHandlers.js
+++ src/actor/slack/SlackHandlers.js
@@ -286,7 +286,7 @@ export class SlackHandlers {
     this.web.chate.postMessage({
       channel: request.channel || this.botChannelId,
       as_user: true,
-      message:
+      text:
         request.message ||
         ":dumpster_fire: Channel notification requested without specifying content.",
     })
