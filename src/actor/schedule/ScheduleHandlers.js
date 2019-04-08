ca412145f9e5b14c7282d357bd147855ad05b55e
Breanna Anderson <breannaa@brownpapertickets.com>
2019-04-08 16:39:45 -0700

    daemon and utility methods for queue

--- src/actor/schedule/ScheduleHandlers.js
+++ src/actor/schedule/ScheduleHandlers.js
@@ -6,8 +6,11 @@ export class ScheduleHandlers {
     this.db = container.db
     this.log = container.log
     this.buildIdSeqKey = "buildIdSeq"
-    this.runDaemon = true // on by default
+    this.runDaemon = true // the damon should run whenever there is work to do in the queue.
     this.daemonStatus = "stopped"
+    this.daemonTimer = null
+    this.daemonInterval = 5000 // milliseconds
+    this.runnigBuild = null
   }
 
   async init() {
@@ -27,6 +30,7 @@ export class ScheduleHandlers {
     const BuildRequest = this.db.BuildRequest
     try {
       const newBuildRequest = await BuildRequest.create(buildData)
+      this._startDaemon() // ensure the daemon is running if enabled.
       return { success: true, message: "success", data: newBuildRequest }
     } catch (ex) {
       return { success: false, message: ex.message }
@@ -46,29 +50,60 @@ export class ScheduleHandlers {
    * Stop the build queue daemon so that no more builds will start. This Does Not stop a running build
    */
   async pauseBuildQueue() {
-    return { status: "paused", length: 5 }
     this.runDaemon = false
+    this._stopDaemon()
+    return { status: "paused", length: 5 }
   }
 
   /**
    * Start the queue daemon if it is stopped
    */
   async startBuildQueue() {
-    return { status: "running", length: 5 }
     this.runDaemon = true
+    this._startDaemon()
+    return { status: "running", length: 5 }
   }
 
-  async getCurrentBuild() {
-    return {
-      buildId: 205,
-      status: "building",
-      data: { user: "me", type: "pullRequest", repository: "it" },
+  async getNextBuild() {
+    return await this._getNextBuild("queued")
+  }
+
+  async getRunningBuild() {
+    return await this._getNextBuild("running")
+  }
+
+  async _getNextBuild(status) {
+    const filter = { status: { $eq: status } }
+    const sortSpec = { buildId: 1 }
+    const BuildRequest = this.db.BuildRequest
+
+    try {
+      const query = BuildRequest.find(filter)
+        .skip(0)
+        .limit(1)
+        .sort(sortSpec)
+      const result = await query.exec()
+      return { success: true, message: "", data: result[0] }
+    } catch (ex) {
+      return { success: false, message: ex.message, data: null }
     }
   }
 
   /** Get the number of items in the queue (by status or all by default) */
-  async getQueueLength(status = "*") {
-    return { length: 5, status: "running", runningBuildId: 205 }
+  async getQueueLength(request) {
+    const { status } = request
+    let filter = {}
+    if (status != "") {
+      filter["status"] = { $eq: status }
+    }
+    const BuildRequest = this.db.BuildRequest
+    try {
+      const query = BuildRequest.find(filter).count()
+      const result = await query.exec()
+      return { success: true, message: "success", data: { count: result } }
+    } catch (ex) {
+      return { success: false, message: ex.message }
+    }
   }
 
   /**
@@ -78,8 +113,32 @@ export class ScheduleHandlers {
    * @param {*} offset
    * @param {*} limit
    */
-  async listBuildQueue(purpose = "*", status = "*", offset, limit) {
-    return { offset: offset, total: 0, builds: [] }
+  async listBuildQueue(request) {
+    const { purpose, status, offset, limit } = request
+    let find = {}
+    if (purpose != "") {
+      find["purpose"] = { $eq: purpose }
+    }
+    if (status != "") {
+      find["status"] = { $eq: status }
+    }
+    const BuildRequest = this.db.BuildRequest
+    // this.log.info(`listBuildQueue find:
+    // purpose:${purpose}  status:${status} find: ${JSON.stringify(
+    //   find,
+    //   null,
+    //   2
+    // )}`)
+    try {
+      const query = BuildRequest.find(find)
+        .skip(offset)
+        .limit(limit)
+        .sort({ buildId: 1 })
+      const result = await query.exec()
+      return { success: true, message: "success", data: result }
+    } catch (ex) {
+      return { success: false, message: ex.message }
+    }
   }
 
   /**
@@ -110,5 +169,41 @@ export class ScheduleHandlers {
     return seqOut.seq
   }
 
-  async _startDaemon() {}
+  // Daemon methods and handlers ==========================================
+  async _startDaemon() {
+    if (this.runDaemon) {
+      if (this.daemonStatus != "running") {
+        this.daemonTimer = setInterval(
+          this._onDaemonTimer,
+          this.daemonInterval,
+          this
+        )
+        this.daemonStatus = "running"
+        this.log.info(
+          `Daemon started with interval: ${this.daemonInterval} milliseconds`
+        )
+      } else {
+        this.log.info("Daemon is already running. Request ignored.")
+      }
+
+      return true
+    } else {
+      this.log.warning(
+        "The Daemon is disabled. Run 'startBuildQueue' to enable it."
+      )
+      this.daemonStatus = "stopped"
+      return false
+    }
+  }
+
+  async _stopDaemon() {
+    clearInterval(this.daemonTimer)
+    this.log.info("Daemon Stopped")
+    this.daemonStatus = "stopped"
+  }
+
+  async _onDaemonTimer(origin) {
+    const now = new Date()
+    this.log.info(`onDaemonTimer ${now.toISOString()}`)
+  }
 }
--- src/actor/web/routes/ScheduleRoutes.js
+++ src/actor/web/routes/ScheduleRoutes.js
@@ -12,6 +12,10 @@ export class ScheduleRoutes {
 
     app.route("/buildQueue/queueBuild").post(this.queueBuild)
     app.route("/buildQueue/stopBuild/:buildId").post(this.stopBuild)
+    app.route("/buildQueue").get(this.listBuildQueue)
+    app.route("/buildQueue/length").get(this.getQueueLength)
+    app.route("/buildQueue/next").get(this.getNextBuild)
+    app.route("/buildQueue/running").get(this.getRunningBuild)
   }
 
   async queueBuild(req, res, next) {
@@ -31,5 +35,57 @@ export class ScheduleRoutes {
     const id = req.params.id
     this.log.info(`stopBuild id: ${id}`)
     res.json({ success: true })
+  }
+
+  async listBuildQueue(req, res, next) {
+    let offset = req.query.offset || 0
+    offset = parseInt(offset)
+
+    let limit = req.query.limit || 100
+    limit = parseInt(limit)
+    const purpose = req.query.purpose || ""
+    const status = req.query.status || ""
+
+    this.log.info(`listBuildQueue: ${JSON.stringify(req.query)}`)
+    const request = { offset, limit, purpose, status }
+    const reply = await this.scheduleMQ.requestAndReply(
+      this.scheduleExchange,
+      "listBuildQueue",
+      request
+    )
+    this.log.info(`list reply: ${JSON.stringify(reply)}`)
+    res.json(reply.reply)
+  }
+
+  async getQueueLength(req, res, next) {
+    const status = req.query.status || ""
+
+    const request = { status }
+    const reply = await this.scheduleMQ.requestAndReply(
+      this.scheduleExchange,
+      "getQueueLength",
+      request
+    )
+    res.json(reply.reply)
+  }
+
+  async getNextBuild(req, res, next) {
+    const request = {}
+    const reply = await this.scheduleMQ.requestAndReply(
+      this.scheduleExchange,
+      "getNextBuild",
+      request
+    )
+    res.json(reply.reply)
+  }
+
+  async getRunningBuild(req, res, next) {
+    const request = {}
+    const reply = await this.scheduleMQ.requestAndReply(
+      this.scheduleExchange,
+      "getRunningBuild",
+      request
+    )
+    res.json(reply.reply)
   }
 }
